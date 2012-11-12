b686aa9f2b5608d5417da52be0ef0c5d5c51f4e1
Florian Flachsenberg <Flachsenberg@gmail.com>
2012-11-12 14:02:20 +0100

    N/A

--- Uebung04/a43.rb
+++ Uebung04/a43.rb
@@ -1,11 +1,21 @@
 #! /usr/bin/env ruby
 
-n = 1000
+if ARGV.empty?
+  n = 1000
+else
+  begin
+    n = Integer(ARGV[0])
+  rescue
+    puts "FEHLER: #{ARGV[0]} ist keine ganze Zahl"
+    exit 1
+  end
+end
+
 erathostenes = Array.new n+1, true
 erathostenes[0] = false
 erathostenes[1] = false
 
-2.upto(sqrt(n)) do |i|
+2.upto(Math.sqrt(n)) do |i|
  if erathostenes[i]
   2.upto(n/i) do |j|
    erathostenes[i*j] = false
@@ -13,4 +23,14 @@ erathostenes[1] = false
  end
 end
 
+counter = 0
+erathostenes.each_index.reverse_each do |i|
+  if erathostenes[i]
+    counter += 1
+    if counter <= 5
+      puts "#{counter}. groesste Primzahl <= #{n}: #{i}"
+    end
+  end
+end
 
+puts "Es gibt #{counter} Primzahlen <= #{n}"
